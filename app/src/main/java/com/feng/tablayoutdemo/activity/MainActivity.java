18ce6803e8c0c6203ab3734a939ff7730e6a09d9
hamor <hamor.feng@zen-game.cn>
2023-06-02 12:21:46 +0800

    Android测试demo

---
+++ app/src/main/java/com/feng/tablayoutdemo/activity/MainActivity.java
@@ -0,0 +1,30 @@
+package com.feng.tablayoutdemo.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.feng.tabdemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initData();
+    }
+
+    private void initView() {
+
+    }
+
+    private void initData() {
+
+    }
+}
